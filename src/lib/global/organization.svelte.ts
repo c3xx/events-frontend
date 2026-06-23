e5107ebd4cef06f33ecbecf2ac160ef49a7b0646
nearHaan <farhaan.nizam675@gmail.com>
2026-06-24 01:33:21 +0530

    deleted unnecessary gloabal state variables

--- src/lib/global/organization.svelte.ts
+++
@@ -1,9 +0,0 @@
-import type { Organization } from '$lib/types';
-
-export const addOrganizationState = $state<{
-	selectedOrganization: Organization | null;
-	sheetOpen: boolean;
-}>({
-	selectedOrganization: null,
-	sheetOpen: false
-});
--- src/lib/global/venueFacilities.svelte.ts
+++
@@ -1,6 +0,0 @@
-import type { Venue } from '$lib/types';
-
-export const venueFacilitiesState = $state<{ selectedVenue: Venue | null; sheetOpen: boolean }>({
-	selectedVenue: null,
-	sheetOpen: false
-});
