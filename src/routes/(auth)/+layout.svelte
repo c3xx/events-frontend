10edbf4cedbb04c200cff5c73efeea4075d6915e
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-06-28 04:26:10 +0530

    Simplify the header

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { logout } from '$lib/api/auth';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { LogOut } from '@lucide/svelte';
 
 	let { children } = $props();
 </script>
@@ -13,17 +10,10 @@
 	<Sidebar.Inset class="flex-1">
 		<main class="h-screen min-w-0 overflow-y-auto">
 			<div
-				class="border-muted-background sticky top-0 z-50 flex h-12 w-full items-center border-b bg-background p-xxxs"
+				class="sticky top-0 z-50 flex h-12 w-full items-center gap-2 border-b bg-background/50 px-3 backdrop-blur-lg"
 			>
-				<Sidebar.Trigger class="absolute left-xs z-50" />
-				<h2 class="absolute inset-0 flex w-full items-center justify-center text-center">Events</h2>
-				<button
-					onclick={async () => {
-						await logout();
-						await goto('/login');
-					}}
-					class="absolute right-xs z-50 p-xxs text-red-700 sm:hidden"><LogOut size="15" /></button
-				>
+				<Sidebar.Trigger />
+				<div>Events</div>
 			</div>
 			{@render children?.()}
 		</main>
